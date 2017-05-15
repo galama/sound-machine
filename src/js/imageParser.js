ee11f883a034375ac0f47cfa729386e614b3a5a1
Nemanja Lazic <nemanja.lazic@devtech.local>
2017-05-15 12:07:52 +0200

    minor

--- src/js/imageParser.js
+++ src/js/imageParser.js
@@ -2,10 +2,11 @@ $("#processbtn").prop('disabled', true);
 
 loadImageToCanvas = function(e) {
     var reader = new FileReader();
-
+    $("#imgwrapper").empty();
     reader.onload = function(event) {
         var img = new Image();
-        
+        var button = document.getElementById('processbtn');
+
         img.onload = function() {
 
 			var me = this;
@@ -20,8 +21,28 @@ loadImageToCanvas = function(e) {
 			$("#imgwrapper")
 				.width(me.width)
 				.height(me.height)
-				.resizable()
-				.draggable();
+
+            var image = document.querySelector('#image');
+            var cropper = new Cropper(image, {
+                movable: false,
+                zoomable: false,
+                rotatable: false,
+                scalable: false
+            });
+
+            button.onclick = function () {
+                var croppedCanvas;
+                croppedCanvas = cropper.getCroppedCanvas();
+                $("#imgwrapper").empty();
+                jQuery('<img/>', {
+                    src: croppedCanvas.toDataURL(),
+                    id: "image",
+                    class: 'image',
+                    style: "width:100%; height:100%"
+                }).appendTo('#imgwrapper');
+
+                process();
+            };
 
             $("#processbtn").prop('disabled', false);
         };
@@ -35,19 +56,3 @@ loadImageToCanvas = function(e) {
 /*** EVENTS ***/
 var imageLoader = document.getElementById('imageLoader');
     imageLoader.addEventListener('change', loadImageToCanvas, false);
-
-$("#svg")
-.mouseout(function() {
-	$("#canvas").css('z-index', -10);
-})
-.mouseover(function() {
-	$("#canvas").css('z-index', 10);
-});
-
-$("#canvas")
-.mouseover(function() {
-	$(this).css('z-index', 10);
-})
-.mouseout(function() {
-	$(this).css('z-index', -10);
-});
--- src/js/main.js
+++ src/js/main.js
@@ -20,25 +20,35 @@ const GRID_CELLS = 9;
  * @type {number}
  * Grid start X
  */
-const  GRID_START_X = 112;
+const GRID_START_X = 83;
 
 /**
  * @type {number}
  * GRID start Y
  */
-const  GRID_START_Y = 62;
+const GRID_START_Y = 69;
 
 /**
  * @type {number}
  * cell width
  */
-const  CELL_WIDTH = 97;
+const CELL_WIDTH = 63;
 
 /**
  * @type {number}
  * cell height
  */
-const  CELL_HEIGHT = 37;
+const CELL_HEIGHT = 37;
+
+const CELL_BORDER = 3;
+
+
+// function getColor(context, img, x,y) {
+//
+//     cellData = context.getImageData(x, y, img.width, img.height);
+//
+//
+// }
 
 
 function process() {
@@ -49,37 +59,77 @@ function process() {
 
     var canvas = document.createElement('canvas');
     var context = canvas.getContext('2d');
-    var img = document.getElementById('image');
-    canvas.width = img.width;
-    canvas.height = img.height;
-    context.drawImage(img, 0, 0 );
+    var tempImg = document.getElementById('image');
+
+    canvas.width = tempImg.width;
+    canvas.height = tempImg.height;
+
+    var img = new Image();
+    img.src = tempImg.src;
+    img.onload = function()
+    {
+        context.drawImage(img, 0, 0);
+
+        for (var i = 0; i < GRID_CELLS; i++) {
+            grid[i] = [];
+            for (var j = 0; j < GRID_CELLS; j++) {
+
+                x = GRID_START_X + CELL_WIDTH * j + CELL_WIDTH / 2 + CELL_BORDER;
+                y = GRID_START_Y + CELL_HEIGHT * i + CELL_HEIGHT / 2 + CELL_BORDER;
 
 
-    for (var i = 0; i <= GRID_CELLS; i++) {
-        grid[i] = [];
+                cellData = context.getImageData(x, y, tempImg.width, tempImg.height);
+
+                context.rect(x,y,2,2);
+                context.fillStyle = "red";
+                context.fill();
+
+                grid[i][j] = {
+                    r: cellData.data[0],
+                    g: cellData.data[1],
+                    b: cellData.data[2],
+                    a: cellData.data[3]
+                }
+            }
+        }
+
+        drowTable(grid, tempImg);
+    };
+}
+
+function drowTable(grid, tempImg) {
+
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+
+    canvas.width = tempImg.width;
+    canvas.height = tempImg.height;
+
+    html = "";
+
+    for (var i = 0; i < GRID_CELLS; i++) {
         for (var j = 0; j < GRID_CELLS; j++) {
 
-            x = GRID_START_X + CELL_WIDTH * j +  CELL_WIDTH / 2;
-            y = GRID_START_Y + CELL_HEIGHT * i +  CELL_HEIGHT / 2;
+            x = GRID_START_X + CELL_WIDTH * j + CELL_WIDTH / 2 + CELL_BORDER;
+            y = GRID_START_Y + CELL_HEIGHT * i + CELL_HEIGHT / 2 + CELL_BORDER;
 
-            /**
-             * debug dots
-             */
-            jQuery('<div>', {
-                class: 'dot',
-                style: "top:" + y +"px; left:" + x +"px"
-            }).appendTo('#imgwrapper');
-            
-            cellData = context.getImageData(x, y, img.width, img.height);
 
-            grid[i][j] = {
-                r: cellData.data[0],
-                g: cellData.data[1],
-                b: cellData.data[2],
-                a: cellData.data[3]
-            }
+            html += "<span style='background: " + getColor(grid[i][j]) + "'>test</span>"
+            //
+            // context.rect(x,y,CELL_WIDTH,CELL_HEIGHT);
+            // context.fillStyle = getColor(grid[i][j]);
+            // context.fill();
+
         }
     }
 
-    console.log(grid);
+    document.body.innerHTML = html;
+}
+
+function getColor(color) {
+
+    return "rgba(" + color.r +',' + color.g +',' + color.b +',' + color.a + ');'
+
+
+
 }
